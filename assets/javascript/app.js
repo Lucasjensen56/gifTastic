36e85ffa6a38b481791de881be6460c9f818cb88
Lucas Jensen <lucasjensen56@gmail.com>
2018-03-19 18:53:44 -0500

    created form in index.html file -- started creating ids to use for later js

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+
+
+
+ // var searchInput = something //something goes here 
+
+ var gifSearch;
+
+// var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifSearch + "&api_key=PjsJJF5igGmYhSxudBVlGWyciIlt43Qz"
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=harry+potter&api_key=PjsJJF5igGmYhSxudBVlGWyciIlt43Qz"
+
+
+$.ajax({
+	url: queryURL,
+	method: "GET"
+}).then(function(response) {
+	console.log(response);
+}) 
