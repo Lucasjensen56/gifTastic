824f9c377b6914c34d2f68b6820c7d8f423fff82
Lucas Jensen <lucasjensen56@gmail.com>
2018-03-23 11:57:59 -0500

    Update app.js
    gifs now come in still and animate when you click on them

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,46 @@
 // empty gif array 
 var gifsArray = [];
 
+var topics = ["voldemort", "harry Potter", "ron", "star wars"]
+
+function displayInitialButtons() {
+
+  for (var i = 0; i < topics.length; i++) {
+    
+    
+  var topicsDiv = $("<button>");
+  var topicsBtnName = topicsDiv.text(topics[i]);
+     
+    
+  //var topicsVal = topics[i];
+    
+    //var topicsVal = $(this).topics[i];
+  
+   $("#initial-buttons").append(topicsDiv)
+    
+    
+  
+    
+    
+    $("button").on("click", function() {
+      //console.log($(this).val())
+       displayGifs($("#topicsDiv").val())  
+      
+    });
+    
+   
+   
+  }
+
+}
+
+
+
+
+ 
+
 function displayGifs(gifSearch) {
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifSearch +    "&api_key=PjsJJF5igGmYhSxudBVlGWyciIlt43Qz&limit=5"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifSearch +    "&api_key=PjsJJF5igGmYhSxudBVlGWyciIlt43Qz&limit=10"
 
   $.ajax({
     url: queryURL,
@@ -19,13 +57,19 @@ function displayGifs(gifSearch) {
 
       var rating = $("<p>").text("Rating: " + results[i].rating) 
 
-      console.log(results[i].rating)
+     // console.log(results[i].rating)
 
       var gifImage = $("<img>");
 
-
-      gifImage.attr("src", results[i].images.fixed_height.url);
-      gifImage.attr("gif");
+      // gifImage.attr("src", results[i].images.fixed_height.url);
+      
+      gifImage.attr("src", results[i].images.fixed_height_still.url);
+      
+      gifImage.attr("data-still",results[i].images.fixed_height_still.url);
+      
+      gifImage.attr("data-animate", results[i].images.fixed_height.url);
+            
+      gifImage.addClass("gif");
 
       gifDiv.append(rating);
       
@@ -38,6 +82,23 @@ function displayGifs(gifSearch) {
 
         $("#gifsDisplay").prepend(gifDiv);
       //$("#gifsDisplay").prepend(gifImage);
+      
+      $(".gif").on("click", function() {
+          console.log("i cliked on the gif")
+         
+  
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+
+       
+
+    });
  
 
 	};
@@ -50,19 +111,24 @@ function createButtons() {
 
 
 $("gifsButtons").empty();
+  
+
 
 
 $("#gifSearch-input").on("click", function (event) {
   event.preventDefault();
-
+  
 	var addedGifs = $("#gifSearch").val().trim();
-
+  
 	gifsArray.push(addedGifs)
 	var addedGifsButton = $("<button>");
 	addedGifsButton.append(addedGifs);
 	$("#gifsButtons").prepend(addedGifsButton)
 
 	addedGifsButton.addClass("gif")
+  
+  
+
   
     $("button").on("click", function() {
       
@@ -84,9 +150,8 @@ $("#gifSearch-input").on("click", function (event) {
 
 
 }
-
+displayInitialButtons()
 
 $(document).on("click", createButtons())
 // createButtons()
-
 
