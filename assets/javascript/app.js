d4fdd095051836e9bd434dfa71623034b87edc49
Lucas Jensen <lucasjensen56@gmail.com>
2018-03-23 07:57:37 -0500

    Update app.js
    created function within createButtons function so any button generated search for the specific gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@
 
 
 
+
 // empty gif array 
 
 var gifsArray = [];
@@ -42,7 +43,7 @@ $.ajax({
 	gifImage.append(gifDiv);
 
 	// $("#gifsDisplay").empty();
-	$("#gifsDisplay").append(gifImage);
+	$("#gifsDisplay").prepend(gifImage);
 
 
 
@@ -60,10 +61,8 @@ $("gifsButtons").empty();
 
 
 $("#gifSearch-input").on("click", function (event) {
+  event.preventDefault();
 
-
-	    event.preventDefault();
- 
 	var addedGifs = $("#gifSearch").val().trim();
 
 	gifsArray.push(addedGifs)
@@ -72,13 +71,19 @@ $("#gifSearch-input").on("click", function (event) {
 	$("#gifsButtons").prepend(addedGifsButton)
 
 	addedGifsButton.addClass("gif")
+  
+    $("button").on("click", function() {
+      
+      displayGifs(addedGifs)
+      
+    })
 
 
 
 
 // commented this out to stop displays from generating every time I just click the search button. We need the buttons to
 
-displayGifs(addedGifs)
+//displayGifs(addedGifs)
 
 
 
@@ -92,116 +97,3 @@ displayGifs(addedGifs)
 $(document).on("click", createButtons())
 // createButtons()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // empty gif array 
-
-// var gifsArray = [];
-
-
-
-
-//  var gifSearch;
-
-// function displayGifs() {
-
-
-// 	 // var gifSearch = $(this).attr("data-results");
-
-// 	 // var gifSearch = "Harry Potter"
-
-// 	 // var gifSearch;
-
-
-// 	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifSearch + "&api_key=PjsJJF5igGmYhSxudBVlGWyciIlt43Qz&limit=5"
-
-// // var queryURL = "https://api.giphy.com/v1/gifs/search?q=Harry+Potter&api_key=PjsJJF5igGmYhSxudBVlGWyciIlt43Qz&limit=5"
-
-
-
-// $.ajax({
-// 	url: queryURL,
-// 	method: "GET"
-// }).then(function(response) {
-
-// 	// console.log(response.data);
-
-
-// 	var results = response.data
-
-// 	for (var i = 0; i < results.length; i++) {
-
-
-// 	var gifDiv = $("<div>");
-
-// 	// var test = $("<p>").text(JSON.stringify(response))
-
-// 	var gifImage = $("<img>");
-
-// 	gifImage.attr("src", results[i].images.fixed_height.url);
-
-
-
-// 	gifImage.append(gifDiv);
-
-
-// 	$("#gifsDisplay").prepend(gifImage);
-
-// };
-
-
-// }) 
-
-
-// }
-
-
-// function createButtons() {
-
-// 	$("gifsButtons").empty();
-
-
-
-// // grabbing input from user
-// 	$("#gifSearch-input").on("click", function(event) {
-// 		 event.preventDefault();
-
-// 		var addedGifs = $("#gifSearch").val().trim();
-
-
-	
-
-
-
-// 		gifsArray.push(addedGifs)
-
-// 		var addedGifsButton = $("<button>");
-// 		addedGifsButton.attr("data-results")
-// 		addedGifsButton
-
-
-// 		addedGifsButton.append(addedGifs);
-
-// 		$("#gifsButtons").prepend(addedGifsButton)
-// 	})
-
-// }
-
-// $(document).on("click", "button",displayGifs)
-
-
-
-// createButtons()
